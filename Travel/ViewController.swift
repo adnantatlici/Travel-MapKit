52ccab34922b28197b2e8073fa3adbe67f0ae90e
Mustafa Adnan Tatlici <m.adnantatlici@gmail.com>
2023-01-13 00:33:18 +0300

    MapKitView

--- Travel/ViewController.swift
+++ Travel/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapKitView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    
 }
 
