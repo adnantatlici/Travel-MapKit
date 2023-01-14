a7cd0d05c61b3a784c9a5704634dcd01e09c795c
Mustafa Adnan Tatlici <m.adnantatlici@gmail.com>
2023-01-14 23:22:05 +0300

    gestureRecognizer MKPointAnnotation

--- Travel/ViewController.swift
+++ Travel/ViewController.swift
@@ -23,8 +23,37 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 1
+        mapKitView.addGestureRecognizer(gestureRecognizer)
     }
 
+    @objc func chooseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == .began {
+            let touchedPoint = gestureRecognizer.location(in: self.mapKitView)
+            
+            let touchedCoordinates = self.mapKitView.convert(touchedPoint, toCoordinateFrom: self.mapKitView)
+            
+            
+            
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = touchedCoordinates
+            annotation.title = "New Annotation"
+            annotation.subtitle = "Travel Book"
+            self.mapKitView.addAnnotation(annotation)
+        }
+     
+        
+        
+        
+        
+    }
+    
+    
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
